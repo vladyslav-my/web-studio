c335fc05709c81f623b6e8c1a8d9ae24d9a6968e
Vladislav <vladisl13777@gmail.com>
2023-10-21 03:48:52 +0300

    Created svg sprites. Saved all icons, image. Created finally html

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	publicDir: 'src/',
 	plugins: [
 		ViteEjsPlugin(),
 	],
 	resolve: {
-		alias: [{ find: "@", replacement: "/src" }],
+		alias: 
+		[
+			{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
+		],
+		
 	},
 	css: {},
 	define: {},
